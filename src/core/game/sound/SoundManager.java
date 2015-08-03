ab48ba28c2538dd72d620e04a133fb5cfbb9dc38
7Winds <nshusa99@gmail.com>
2015-08-03 04:19:28 -0500

    A basis for a sound effects system.

---
+++ src/core/game/sound/SoundManager.java
@@ -0,0 +1,363 @@
+package core.game.sound;
+
+import core.Config;
+import core.game.model.entity.player.Equipment;
+import core.game.model.entity.player.Player;
+import core.game.model.item.ItemTableManager;
+
+/**
+ * 
+ * @author FUZENSETH
+ *
+ */
+public class SoundManager {
+
+	/**
+	 * The sound types.
+	 * 
+	 * @author FUZENSETH
+	 *
+	 */
+	public enum SoundType {
+		MELEE_COMBAT, NPC_SOUND, REGION_MUSIC, CAST_SOUND, MAGIC_COMBAT, RANGED_COMBAT, SKILL_SOUND, PLAYER_BLOCK, NPC_BLOCK, SPECIAL_ATTACK;
+	}
+
+	/**
+	 * Sends a sound
+	 * 
+	 * @param p
+	 * @param id
+	 * @param type
+	 * @return
+	 */
+	public static final int sendSound(Player p, int id, SoundType type) {
+		switch (type) {
+		/**
+		 * RANGE COMBAT SOUNDS so basically if i want to add new bow soudn
+		 */
+		case RANGED_COMBAT:
+			String bow = Equipment.getItemName(id).toLowerCase();
+			if (bow.contains("shortbow") || bow.contains("longbow"))
+				p.getPlayerAssistant().sendSound(370);
+			else if (bow.contains("knife") || bow.contains("dart"))
+				p.getPlayerAssistant().sendSound(364);
+			else if (bow.contains("c'bow")) // <.. item name contains
+				p.getPlayerAssistant().sendSound(364);// <-- sends the sound by
+														// given id
+
+			break;
+		/**
+		 * BLOCK SOUNDS WHEN PLAYER GETS A HIT
+		 * 
+		 */
+		case PLAYER_BLOCK:
+			// String shield = p.getItems()
+			// .getItemName(p.playerEquipment[p.playerShield]).toLowerCase();
+			String shield = Equipment.getItemName(id).toLowerCase();
+			if (shield.contains("kiteshield")
+					|| shield.contains("toktz-ket-xil"))
+				p.getPlayerAssistant().sendSound(414);
+			else if (shield.contains("defender"))
+				p.getPlayerAssistant().sendSound(409);
+			break;
+		/**
+		 * THE CASTING SOUND WHEN YOU START CASTING A SPELL.
+		 */
+		case CAST_SOUND:
+			switch (id) { // switches spell id
+
+			case 0:// Air strike
+				p.getPlayerAssistant().sendSound(220);
+				break;
+			case 1:// Water strike
+				p.getPlayerAssistant().sendSound(211);
+				break;
+			case 2:// Earth strike
+				p.getPlayerAssistant().sendSound(1002);
+				break;
+			case 3:// Fire strike
+				p.getPlayerAssistant().sendSound(1017);
+				break;
+			case 4:// Air bolt
+				p.getPlayerAssistant().sendSound(218);
+				break;
+			case 5:// Water bolt
+				p.getPlayerAssistant().sendSound(211);
+				break;
+			case 6:// Earth bolt
+				p.getPlayerAssistant().sendSound(1003);
+				break;
+			case 7:// Fire bolt
+				p.getPlayerAssistant().sendSound(1015);
+				break;
+			case 8:// Air blast
+				p.getPlayerAssistant().sendSound(216);
+				break;
+			case 9:// Water blast
+				p.getPlayerAssistant().sendSound(207);
+				break;
+			case 10:// Earth blast
+				p.getPlayerAssistant().sendSound(1007);
+				break;
+			case 11:// Fire blast
+				p.getPlayerAssistant().sendSound(1020);
+				break;
+			case 12:// Air wave
+				p.getPlayerAssistant().sendSound(222);
+				break;
+			case 13:// Water wave
+				p.getPlayerAssistant().sendSound(213);
+				break;
+			case 14:// Earth wave
+				p.getPlayerAssistant().sendSound(1009);
+				break;
+			case 15:// Fire wave
+				p.getPlayerAssistant().sendSound(1014);
+				break;
+			case 32:
+				p.getPlayerAssistant().sendSound(1114);
+				break;
+			case 33:
+			case 34:
+				p.getPlayerAssistant().sendSound(986);
+				break;
+			case 35:
+				p.getPlayerAssistant().sendSound(1112);
+				break;
+			case 36:
+				p.getPlayerAssistant().sendSound(1115);
+				break;
+			case 37:
+				p.getPlayerAssistant().sendSound(1014);
+				break;
+			case 38:
+				p.getPlayerAssistant().sendSound(986);
+				break;
+			case 39:
+				p.getPlayerAssistant().sendSound(1126);
+				break;
+			case 40:
+				p.getPlayerAssistant().sendSound(1116);
+				break;
+			case 41:
+			case 42:
+				p.getPlayerAssistant().sendSound(986);
+				break;
+			case 43:
+				p.getPlayerAssistant().sendSound(1110);
+				break;
+			case 44:
+			case 45:
+			case 46:
+			case 47:
+				p.getPlayerAssistant().sendSound(1111);
+				break;
+			// spells
+
+			}
+			break;
+
+		/**
+		 * Magic sounds when it reaches the target.
+		 */
+		case MAGIC_COMBAT:
+			switch (id) {
+
+			// spells
+			case 0:// Air strike
+				p.getPlayerAssistant().sendSound(221);
+				break;
+			case 1:// Water strike
+				p.getPlayerAssistant().sendSound(212);
+				break;
+			case 2:// Earth strike
+				p.getPlayerAssistant().sendSound(1004);
+				break;
+			case 3:// Fire strike
+				p.getPlayerAssistant().sendSound(1018);
+				break;
+			case 4:// Air bolt
+				p.getPlayerAssistant().sendSound(219);
+				break;
+			case 5:// Water bolt
+				p.getPlayerAssistant().sendSound(210);
+				break;
+			case 6:// Earth bolt
+				p.getPlayerAssistant().sendSound(1006);
+				break;
+			case 7:// Fire bolt
+				p.getPlayerAssistant().sendSound(1016);
+				break;
+			case 8:// Air blast
+				p.getPlayerAssistant().sendSound(217);
+				break;
+			case 9:// Water blast
+				p.getPlayerAssistant().sendSound(208);
+				break;
+			case 10:// Earth blast
+				p.getPlayerAssistant().sendSound(1005);
+				break;
+			case 11:// Fire blast
+				p.getPlayerAssistant().sendSound(1019);
+				break;
+			case 12:// Air wave
+				p.getPlayerAssistant().sendSound(223);
+				break;
+			case 13:// Water wave
+				p.getPlayerAssistant().sendSound(214);
+				break;
+			case 14:// Earth wave
+				p.getPlayerAssistant().sendSound(1008);
+				break;
+			case 15:// Fire wave
+				p.getPlayerAssistant().sendSound(1021);
+				break;
+			case 47:
+				p.getPlayerAssistant().sendSound(1125);
+				break;
+			case 39:
+				p.getPlayerAssistant().sendSound(1126);
+				break;
+			case 43:
+				p.getPlayerAssistant().sendSound(1110);
+				break;
+			case 32:
+			case 33:
+			case 34:
+			case 35:
+			case 36:
+			case 37:
+			case 38:
+			case 40:
+			case 41:
+			case 42:
+			case 44:
+			case 45:
+			case 46:
+				p.getPlayerAssistant().sendSound(1125);
+				break;
+			}
+			break;
+
+		/**
+		 * Represent's players combat sounds. (when you hit with a weapon)
+		 */
+		case MELEE_COMBAT:
+			final String weaponName = ItemTableManager.forID(id).getName()
+					.toLowerCase();
+			if (weaponName.contains("scimitar"))
+				p.getPlayerAssistant().sendSound(396);
+			else if (weaponName.contains("mace"))
+				p.getPlayerAssistant().sendSound(395);
+			else if (weaponName.contains("dagger"))
+				p.getPlayerAssistant().sendSound(793);
+			else if (weaponName.contains("battleaxe"))
+				p.getPlayerAssistant().sendSound(399);
+			else if (weaponName.contains("2h sword"))
+				p.getPlayerAssistant().sendSound(425);
+			else if (weaponName.contains("flail"))
+				p.getPlayerAssistant().sendSound(1059);
+			switch (id) {// switches weapon id
+			case 4151:
+			case 14004:// tentacle
+				p.getPlayerAssistant().sendSound(1080);
+				break;
+			case 4718:// dh axe etcetc
+				p.getPlayerAssistant().sendSound(1057);
+				break;
+			case 4153:
+				p.getPlayerAssistant().sendSound(1079);
+				break;
+			case 4727:
+				p.getPlayerAssistant().sendSound(1061);
+				break;
+			}
+			break;
+
+		/**
+		 * Plays region music -TODO not added
+		 */
+		case REGION_MUSIC:// regionids
+			break;
+
+		default:
+			break;
+
+		}
+		return id;
+	}
+
+	/**
+	 * Ripped this int from r-s 2 lazy to do
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static int specialSounds(int id) {
+		if (id == 4151) // whip
+		{
+			return 1081;
+		}
+		if (id == 5698 || id == 1215) // dds
+		{
+			return 385;
+		}
+		if (id == 1434)// Mace
+		{
+			return 387;
+		}
+		if (id == 3204) // halberd
+		{
+			return 420;
+		}
+		if (id == 4153) // gmaul
+		{
+			return 1082;
+		}
+		if (id == 7158) // d2h
+		{
+			return 426;
+		}
+		if (id == 4587) // dscim
+		{
+			return 1305;
+		}
+		if (id == 1215) // Dragon dag
+		{
+			return 793;
+		}
+		if (id == 1305) // D Long
+		{
+			return 390;
+		}
+		if (id == 861) // MSB
+		{
+			return 386;
+		}
+		if (id == 11235) // DBow
+		{
+			return 386;
+		}
+		if (id == 6739) // D Axe
+		{
+		}
+		if (id == 1377) // DBAxe
+		{
+			return 389;
+		}
+		return -1;
+	}
+
+	/**
+	 * Playes a sound effect.
+	 * 
+	 * @param player
+	 * @param id
+	 */
+	public static final void play(Player player, int i) {
+		if (!Config.enableSound)
+			return;
+		player.getPlayerAssistant().sendSound(i);
+	}
+
+}
---
+++ src/core/game/sound/effects/SoundEffects.java
@@ -0,0 +1,447 @@
+package core.game.sound.effects;
+
+import core.game.model.entity.npc.NPCDefinitions;
+import core.game.model.entity.player.Player;
+import core.game.model.item.ItemTableManager;
+import core.game.util.Misc;
+
+public class SoundEffects {
+
+	Player player;
+
+	public SoundEffects(Player player) {
+		this.player = player;
+	}
+
+	/**
+	 * Singular sound variables.
+	 */
+
+	public final int LEVELUP = 67;
+	public final int DUELWON = 77;
+	public final int DUELLOST = 76;
+	public final int FOODEAT = 317;
+	public final int DROPITEM = 376;
+	public final int COOKITEM = 357;
+	public final int SHOOT_ARROW = 370;
+	public final int TELEPORTM = 202;
+	public final int BONE_BURY = 380;
+	public final int DRINK_POTION = 334;
+
+	public static int getNpcAttackSounds(int NPCID) {
+		String npc = GetNpcName(NPCID).toLowerCase();
+		if (npc.contains("bat")) {
+			return 1;
+		}
+		if (npc.contains("cow")) {
+			return 4;
+		}
+		if (npc.contains("imp")) {
+			return 11;
+		}
+		if (npc.contains("rat")) {
+			return 17;
+		}
+		if (npc.contains("duck")) {
+			return 26;
+		}
+		if (npc.contains("wolf") || npc.contains("bear")) {
+			return 28;
+		}
+		if (npc.contains("dragon")) {
+			return 47;
+		}
+		if (npc.contains("ghost")) {
+			return 57;
+		}
+		if (npc.contains("goblin")) {
+			return 88;
+		}
+		if (npc.contains("skeleton") || npc.contains("demon")
+				|| npc.contains("ogre") || npc.contains("giant")
+				|| npc.contains("tz-") || npc.contains("jad")) {
+			return 48;
+		}
+		if (npc.contains("zombie")) {
+			return 1155;
+		}
+		if (npc.contains("man") || npc.contains("woman")
+				|| npc.contains("monk")) {
+			return 417;
+		}
+		return Misc.random(6) > 3 ? 398 : 394;
+
+	}
+
+	public static int getNpcBlockSound(int NPCID) {
+		String npc = GetNpcName(NPCID).toLowerCase();
+		if (npc.contains("bat")) {
+			return 7;
+		}
+		if (npc.contains("cow")) {
+			return 5;
+		}
+		if (npc.contains("imp")) {
+			return 11;
+		}
+		if (npc.contains("rat")) {
+			return 16;
+		}
+		if (npc.contains("duck")) {
+			return 24;
+		}
+		if (npc.contains("wolf") || npc.contains("bear")) {
+			return 34;
+		}
+		if (npc.contains("dragon")) {
+			return 45;
+		}
+		if (npc.contains("ghost")) {
+			return 53;
+		}
+		if (npc.contains("goblin")) {
+			return 87;
+		}
+		if (npc.contains("skeleton") || npc.contains("demon")
+				|| npc.contains("ogre") || npc.contains("giant")
+				|| npc.contains("tz-") || npc.contains("jad")) {
+			return 1154;
+		}
+		if (npc.contains("zombie")) {
+			return 1151;
+		}
+		if (npc.contains("man") && !npc.contains("woman")) {
+			return 816;
+		}
+		if (npc.contains("monk")) {
+			return 816;
+		}
+
+		if (!npc.contains("man") && npc.contains("woman")) {
+			return 818;
+		}
+		return 791;
+
+	}
+
+	public static int getNpcDeathSounds(int NPCID) {
+		String npc = GetNpcName(NPCID).toLowerCase();
+		if (npc.contains("bat")) {
+			return 7;
+		}
+		if (npc.contains("cow")) {
+			return 3;
+		}
+		if (npc.contains("imp")) {
+			return 9;
+		}
+		if (npc.contains("rat")) {
+			return 15;
+		}
+		if (npc.contains("duck")) {
+			return 25;
+		}
+		if (npc.contains("wolf") || npc.contains("bear")) {
+			return 35;
+		}
+		if (npc.contains("dragon")) {
+			return 44;
+		}
+		if (npc.contains("ghost")) {
+			return 60;
+		}
+		if (npc.contains("goblin")) {
+			return 125;
+		}
+		if (npc.contains("skeleton") || npc.contains("demon")
+				|| npc.contains("ogre") || npc.contains("giant")
+				|| npc.contains("tz-") || npc.contains("jad")) {
+			return 70;
+		}
+		if (npc.contains("zombie")) {
+			return 1140;
+		}
+		return 70;
+
+	}
+
+	public static String GetNpcName(int NpcID) {
+		return NPCDefinitions.getDefinitions()[NpcID].getName();
+	}
+
+	public static String getItemName(int ItemID) {
+		return ItemTableManager.forID(ItemID).getName();
+	}
+
+	public static int getPlayerBlockSounds(Player player) {
+
+		int blockSound = 511;
+
+		if (player.playerEquipment[player.playerChest] == 2499
+				|| player.playerEquipment[player.playerChest] == 2501
+				|| player.playerEquipment[player.playerChest] == 2503
+				|| player.playerEquipment[player.playerChest] == 4746
+				|| player.playerEquipment[player.playerChest] == 4757
+				|| player.playerEquipment[player.playerChest] == 10330) {// Dragonhide
+																// sound
+			blockSound = 24;
+		} else if (player.playerEquipment[player.playerChest] == 10551 || // Torso
+				player.playerEquipment[player.playerChest] == 10438) {// 3rd age
+			blockSound = 32;// Weird sound
+		} else if (player.playerEquipment[player.playerChest] == 10338 || // 3rd age
+				player.playerEquipment[player.playerChest] == 7399 || // Enchanted
+				player.playerEquipment[player.playerChest] == 6107 || // Ghostly
+				player.playerEquipment[player.playerChest] == 4091 || // Mystic
+				player.playerEquipment[player.playerChest] == 4101 || // Mystic
+				player.playerEquipment[player.playerChest] == 4111 || // Mystic
+				player.playerEquipment[player.playerChest] == 1035 || // Zamorak
+				player.playerEquipment[player.playerChest] == 12971) {// Combat
+			blockSound = 14;// Robe sound
+		} else if (player.playerEquipment[player.playerShield] == 4224) {// Crystal Shield
+			blockSound = 30;// Crystal sound
+		} else if (player.playerEquipment[player.playerChest] == 1101
+				|| // Chains
+				player.playerEquipment[player.playerChest] == 1103
+				|| player.playerEquipment[player.playerChest] == 1105
+				|| player.playerEquipment[player.playerChest] == 1107
+				|| player.playerEquipment[player.playerChest] == 1109
+				|| player.playerEquipment[player.playerChest] == 1111
+				|| player.playerEquipment[player.playerChest] == 1113
+				|| player.playerEquipment[player.playerChest] == 1115
+				|| // Plates
+				player.playerEquipment[player.playerChest] == 1117
+				|| player.playerEquipment[player.playerChest] == 1119
+				|| player.playerEquipment[player.playerChest] == 1121
+				|| player.playerEquipment[player.playerChest] == 1123
+				|| player.playerEquipment[player.playerChest] == 1125
+				|| player.playerEquipment[player.playerChest] == 1127
+				|| player.playerEquipment[player.playerChest] == 4720
+				|| // Barrows armour
+				player.playerEquipment[player.playerChest] == 4728
+				|| player.playerEquipment[player.playerChest] == 4749
+				|| player.playerEquipment[player.playerChest] == 4712
+				|| player.playerEquipment[player.playerChest] == 11720
+				|| // Godwars armour
+				player.playerEquipment[player.playerChest] == 11724
+				|| player.playerEquipment[player.playerChest] == 3140
+				|| // Dragon
+				player.playerEquipment[player.playerChest] == 2615
+				|| // Fancy
+				player.playerEquipment[player.playerChest] == 2653
+				|| player.playerEquipment[player.playerChest] == 2661
+				|| player.playerEquipment[player.playerChest] == 2669
+				|| player.playerEquipment[player.playerChest] == 2623
+				|| player.playerEquipment[player.playerChest] == 3841
+				|| player.playerEquipment[player.playerChest] == 1127) {// Metal armour
+																// sound
+			blockSound = 15;
+		} else {
+			blockSound = 511;
+		}
+		return blockSound;
+	}
+
+	public static int GetWeaponSound(Player player) {
+
+		String wep = getItemName(player.playerEquipment[player.playerWeapon])
+				.toLowerCase();
+
+		if (player.playerEquipment[player.playerWeapon] == 4718) {// Dharok's Greataxe
+			return 1320;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4734) {// Karil's Crossbow
+			return 1081;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4747) {// Torag's Hammers
+			return 1330;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4710) {// Ahrim's Staff
+			return 2555;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4755) {// Verac's Flail
+			return 1323;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4726) {// Guthan's Warspear
+			return 2562;
+		}
+
+		if (player.playerEquipment[player.playerWeapon] == 772
+				|| player.playerEquipment[player.playerWeapon] == 1379
+				|| player.playerEquipment[player.playerWeapon] == 1381
+				|| player.playerEquipment[player.playerWeapon] == 1383
+				|| player.playerEquipment[player.playerWeapon] == 1385
+				|| player.playerEquipment[player.playerWeapon] == 1387
+				|| player.playerEquipment[player.playerWeapon] == 1389
+				|| player.playerEquipment[player.playerWeapon] == 1391
+				|| player.playerEquipment[player.playerWeapon] == 1393
+				|| player.playerEquipment[player.playerWeapon] == 1395
+				|| player.playerEquipment[player.playerWeapon] == 1397
+				|| player.playerEquipment[player.playerWeapon] == 1399
+				|| player.playerEquipment[player.playerWeapon] == 1401
+				|| player.playerEquipment[player.playerWeapon] == 1403
+				|| player.playerEquipment[player.playerWeapon] == 1405
+				|| player.playerEquipment[player.playerWeapon] == 1407
+				|| player.playerEquipment[player.playerWeapon] == 1409
+				|| player.playerEquipment[player.playerWeapon] == 9100) { // Staff wack
+			return 394;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 839
+				|| player.playerEquipment[player.playerWeapon] == 841
+				|| player.playerEquipment[player.playerWeapon] == 843
+				|| player.playerEquipment[player.playerWeapon] == 845
+				|| player.playerEquipment[player.playerWeapon] == 847
+				|| player.playerEquipment[player.playerWeapon] == 849
+				|| player.playerEquipment[player.playerWeapon] == 851
+				|| player.playerEquipment[player.playerWeapon] == 853
+				|| player.playerEquipment[player.playerWeapon] == 855
+				|| player.playerEquipment[player.playerWeapon] == 857
+				|| player.playerEquipment[player.playerWeapon] == 859
+				|| player.playerEquipment[player.playerWeapon] == 861
+				|| player.playerEquipment[player.playerWeapon] == 4734
+				|| player.playerEquipment[player.playerWeapon] == 2023 // RuneC'Bow
+				|| player.playerEquipment[player.playerWeapon] == 4212
+				|| player.playerEquipment[player.playerWeapon] == 4214
+				|| player.playerEquipment[player.playerWeapon] == 4934
+				|| player.playerEquipment[player.playerWeapon] == 9104
+				|| player.playerEquipment[player.playerWeapon] == 9107) { // Bows/Crossbows
+			return 370;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 1363
+				|| player.playerEquipment[player.playerWeapon] == 1365
+				|| player.playerEquipment[player.playerWeapon] == 1367
+				|| player.playerEquipment[player.playerWeapon] == 1369
+				|| player.playerEquipment[player.playerWeapon] == 1371
+				|| player.playerEquipment[player.playerWeapon] == 1373
+				|| player.playerEquipment[player.playerWeapon] == 1375
+				|| player.playerEquipment[player.playerWeapon] == 1377
+				|| player.playerEquipment[player.playerWeapon] == 1349
+				|| player.playerEquipment[player.playerWeapon] == 1351
+				|| player.playerEquipment[player.playerWeapon] == 1353
+				|| player.playerEquipment[player.playerWeapon] == 1355
+				|| player.playerEquipment[player.playerWeapon] == 1357
+				|| player.playerEquipment[player.playerWeapon] == 1359
+				|| player.playerEquipment[player.playerWeapon] == 1361
+				|| player.playerEquipment[player.playerWeapon] == 9109) { // BattleAxes/Axes
+			return 399;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4718
+				|| player.playerEquipment[player.playerWeapon] == 7808) { // Dharok
+																// GreatAxe
+			return 400;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 6609
+				|| player.playerEquipment[player.playerWeapon] == 1307
+				|| player.playerEquipment[player.playerWeapon] == 1309
+				|| player.playerEquipment[player.playerWeapon] == 1311
+				|| player.playerEquipment[player.playerWeapon] == 1313
+				|| player.playerEquipment[player.playerWeapon] == 1315
+				|| player.playerEquipment[player.playerWeapon] == 1317
+				|| player.playerEquipment[player.playerWeapon] == 1319) { // 2h
+			return 425;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 1321
+				|| player.playerEquipment[player.playerWeapon] == 1323
+				|| player.playerEquipment[player.playerWeapon] == 1325
+				|| player.playerEquipment[player.playerWeapon] == 1327
+				|| player.playerEquipment[player.playerWeapon] == 1329
+				|| player.playerEquipment[player.playerWeapon] == 1331
+				|| player.playerEquipment[player.playerWeapon] == 1333
+				|| player.playerEquipment[player.playerWeapon] == 4587) { // Scimitars
+			return 396;
+		}
+		if (wep.contains("halberd")) {
+			return 420;
+		}
+		if (wep.contains("long")) {
+			return 396;
+		}
+		if (wep.contains("knife")) {
+			return 368;
+		}
+		if (wep.contains("javelin")) {
+			return 364;
+		}
+
+		if (player.playerEquipment[player.playerWeapon] == 9110) {
+			return 401;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4755) {
+			return 1059;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4153) {
+			return 1079;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 9103) {
+			return 385;
+		}
+		if (player.playerEquipment[player.playerWeapon] == -1) { // fists
+			return 417;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 2745
+				|| player.playerEquipment[player.playerWeapon] == 2746
+				|| player.playerEquipment[player.playerWeapon] == 2747
+				|| player.playerEquipment[player.playerWeapon] == 2748) { // Godswords
+			return 390;
+		}
+		if (player.playerEquipment[player.playerWeapon] == 4151) {
+			return 1080;
+		} else {
+			return 398; // Daggers(this is enything that isn't added)
+		}
+	}
+
+	public static int specialSounds(int id) {
+		if (id == 4151) // whip
+		{
+			return 1081;
+		}
+		if (id == 5698) // dds
+		{
+			return 793;
+		}
+		if (id == 1434)// Mace
+		{
+			return 387;
+		}
+		if (id == 3204) // halberd
+		{
+			return 420;
+		}
+		if (id == 4153) // gmaul
+		{
+			return 1082;
+		}
+		if (id == 7158) // d2h
+		{
+			return 426;
+		}
+		if (id == 4587) // dscim
+		{
+			return 1305;
+		}
+		if (id == 1215) // Dragon dag
+		{
+			return 793;
+		}
+		if (id == 1305) // D Long
+		{
+			return 390;
+		}
+		if (id == 861) // MSB
+		{
+			return 386;
+		}
+		if (id == 11235) // DBow
+		{
+			return 386;
+		}
+		if (id == 6739) // D Axe
+		{
+		}
+		if (id == 1377) // DBAxe
+		{
+			return 389;
+		}
+		return -1;
+	}
+}
