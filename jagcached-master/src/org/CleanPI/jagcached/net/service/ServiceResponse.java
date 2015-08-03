468006d87837a66f885a0b75762fe8e9c406951b
7Winds <nshusa99@gmail.com>
2015-08-03 18:23:34 -0500

    Clean up

--- jagcached-master/src/org/CleanPI/jagcached/net/service/ServiceResponse.java
+++
@@ -1,9 +0,0 @@
-package org.CleanPI.jagcached.net.service;
-
-/**
- * Represents a response to a service request.
- * @author Graham
- */
-public final class ServiceResponse {
-
-}
--- jagcached-master/start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -cp bin;lib/netty-3.2.jar -server org.CleanPI.jagcached.FileServer
-pause
