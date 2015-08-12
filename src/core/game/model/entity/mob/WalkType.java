6c2a84aa15e7b627826e960ccdbb9af74c608644
7Winds <nshusa99@gmail.com>
2015-08-12 01:21:42 -0500

    Added documentation

--- src/core/game/model/entity/mob/WalkType.java
+++ src/core/game/model/entity/mob/WalkType.java
@@ -1,5 +1,9 @@
 package core.game.model.entity.mob;
 
+/**
+ * Represents a mob facing direction and/or ability to walk in a random direction
+ * @author 7Winds
+ */
 public enum WalkType {
 	RANDOM,
 	NORTH,
