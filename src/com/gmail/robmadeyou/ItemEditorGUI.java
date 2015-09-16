6bde71f2dcacb157d16f4a9150a4b5509cccb88f
robmadeyou <robmadeyou@gmail.com>
2015-09-16 23:37:37 +0100

    Updating Item editor to load more items

--- src/com/gmail/robmadeyou/ItemEditorGUI.java
+++ src/com/gmail/robmadeyou/ItemEditorGUI.java
@@ -1,6 +1,8 @@
 package com.gmail.robmadeyou;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 import core.game.util.JsonLoader;
@@ -15,14 +17,27 @@ import java.util.ArrayList;
 public class ItemEditorGUI extends JFrame
 {
 
+	private static ArrayList<Item> items = new ArrayList<>();
+
 	public ItemEditorGUI() throws HeadlessException
 	{
 		super();
 
 
+		JsonLoader loader = new JsonLoader( "./Data/json/item_definitions.json" )
+		{
+			@Override
+			public void load( JsonObject reader, Gson builder )
+			{
+				Item i = new Item();
+				i.loadItem( reader );
+				items.add( i );
+			}
+		};
+
+		loader.load();
 	}
 
-	private static ArrayList<Item> items;
 	private static ItemEditorGUI gui;
 	public static void main( String[] args )
 	{
@@ -36,21 +51,12 @@ public class ItemEditorGUI extends JFrame
 
 		gui = new ItemEditorGUI();
 
-		gui.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-		gui.setLocation( 400, 400 );
+		gui.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE );
+		gui.setLocation( 400, 400);
 		gui.setSize( 280, 200 );
 		gui.setVisible( true );
 
-		JsonLoader loader = new JsonLoader( "./Data/json/item_definitions.json" )
-		{
-			@Override
-			public void load( JsonObject reader, Gson builder )
-			{
-				System.out.println( "stuff" );
-			}
-		};
 
-		loader.load();
 
 	}
 
@@ -63,16 +69,43 @@ public class ItemEditorGUI extends JFrame
 		private boolean destroyable;
 		private boolean stackable;
 		private int value;
-		private int[] specialPrice;
-		
-		public Item( int id, String texture )
+		private ArrayList<Integer> specialPrice = new ArrayList<>();
+		private int lowAlchemy;
+		private int highAlchemy;
+		private double weight;
+		private boolean noted;
+		private boolean noteable;
+		private int childId;
+		private int parentId;
+		private boolean isTwoHanded;
+		private String equipmentType;
+		private boolean fullBody;
+		private boolean fullHat;
+		private boolean fullMask;
+		private ArrayList<Double> bonus = new ArrayList<>();
+		private ArrayList<Integer> requirement = new ArrayList<>();
+		private ArrayList<String> actions = new ArrayList<>();
+
+		public Item()
 		{
 			super();
 		}
 
-		public void loadItem()
+		public void loadItem( JsonObject reader )
 		{
-
+			this.id = reader.get("id").getAsInt();
+			this.name = reader.get( "name" ).getAsString();
+			this.description = reader.get( "description" ).getAsString();
+			this.tradeable = reader.get( "tradeable" ).getAsBoolean();
+			this.destroyable = reader.get( "destroyable" ).getAsBoolean();
+			this.stackable = reader.get( "stackable" ).getAsBoolean();
+			this.value = reader.get( "value" ).getAsInt();
+			JsonArray specialItems = reader.get( "specialPrice" ).getAsJsonArray();
+			for( JsonElement spec : specialItems )
+			{
+				this.specialPrice.add( spec.getAsInt() );
+			}
+			this.specialPrice = reader.get( "specialPrice" ).getAsJsonArray();
 		}
 	}
 }
